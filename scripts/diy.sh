356b993fc89543de10f81172f421e0c813f8089b
BusDriver <24316853+soundghost@users.noreply.github.com>
2024-01-06 19:30:05 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-#更改默认地址为192.168.6.1
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+#更改默认地址为192.168.9.1
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+
+#添加软件包
+echo 'src-git-full kenzok https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git-full small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+
+#添加360T7 108M 512M-Ram USB支持
+echo '&xhci {' >> target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7-108M.dts
+echo '        mediatek,u3p-dis-msk = <0x0>;' >> target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7-108M.dts
+echo '        phys = <&u2port0 PHY_TYPE_USB2>,' >> target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7-108M.dts
+echo '                   <&u3port0 PHY_TYPE_USB3>;' >> target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7-108M.dts
+echo '        status = "okay";' >> target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7-108M.dts
+echo '};' >> target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7-108M.dts
+
+#添加K2P 32M nand USB支持
+sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
+sed -i 's/"Phicomm K2P";/"Phicomm K2P (32M USB)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
