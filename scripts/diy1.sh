0ac0546daa5200faa43e13ab8071b0b48a81909d
BusDriver <24316853+soundghost@users.noreply.github.com>
2025-09-01 23:18:45 +0800

    Update and rename diy.sh to diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -10,6 +10,8 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/sbwml/luci-app-airconnect.git  package/luci-app-airconnect
 git clone https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
 #添加360T7 108M 512M-Ram USB支持
 # echo '&xhci {' >> target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7-108M.dts
