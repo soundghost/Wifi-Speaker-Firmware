8f6017b6a1bb88d74ab7f2ccc9638c1f770b149e
BusDriver <24316853+soundghost@users.noreply.github.com>
2025-09-09 01:14:46 +0800

    Update diy2.sh

--- scripts/diy2.sh
+++ scripts/diy2.sh
@@ -2,6 +2,7 @@
 
 ##-----------------Del duplicate packages------------------
 rm -rf feeds/packages/net/open-app-filter
+rm -rf feeds/luci/applications/luci-app-appfilter
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
