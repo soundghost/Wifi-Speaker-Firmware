39bc7a495af1c4962c15b57c4dfd816cadc1101c
BusDriver <24316853+soundghost@users.noreply.github.com>
2025-09-11 19:49:51 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -14,15 +14,8 @@ curl -sSL https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/mast
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/mosdns
 
-# 移除 openwrt feeds 自带的核心库
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-
-# 移除 openwrt feeds 过时的luci版本
-rm -rf feeds/luci/applications/luci-app-passwall
-
 #添加软件包
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+git clone https://github.com/badaix/snapos.git
 git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/sbwml/luci-app-airconnect.git  package/luci-app-airconnect
 git clone https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
