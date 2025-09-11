7dacc3966129624e2c24d4deb04239b6e8cf0bf3
BusDriver <24316853+soundghost@users.noreply.github.com>
2025-09-11 22:09:02 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #更改默认地址为192.168.9.1
-sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
 
 #添加软件包
 git clone https://github.com/badaix/snapos.git
